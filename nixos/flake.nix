737169aa633960cd4f4f0acba07ab01db8daec30
EzT32 <theodor.berghansen@icloud.com>
2025-01-14 22:48:49 +0100

    My first commit in my nixos journey :D! Hurraygit add .git add .git add .git add .!

---
+++ nixos/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+
+  in
+  {
+    # This flake defines a single NixOS config named "default" for simplicity, as it's implied in the rebuild-command.
+    # For multiple configurations, modify to the following:
+    # nixosConfigurations = {
+    #   <keyname> = nixpkgs.lib.nixosSystem {
+    #     specialArgs { inherit inputs; };
+    #     
+    #     modules = [
+    #       ./nixos/configuration.nix
+    #       <other modules>
+    #     ];
+    #   };
+    # }:  
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit system; };
+	
+      modules = [
+	./configuration.nix
+      ];
+    };
+  };
+}
