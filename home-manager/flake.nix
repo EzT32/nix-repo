04f8817d3b2bb0cf258efdd7f75359adccd828a8
EzT32 <theodor.berghansen@icloud.com>
2025-03-18 00:44:33 +0100

    Tried cleaning up AndroidStudio mess

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,7 +2,6 @@
   description = "Home Manager configuration of ezt";
 
   inputs = {
-    # Use the latest unstable Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -10,37 +9,24 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";  # Adjust this if using another architecture
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
 
-      # Apply an overlay to customize android-studio
-      overlays = [
-        (final: prev: {
-          android-studio = prev.android-studio.override {
-            forceWayland = false;
-          };
-        })
-      ];
-
-      # Import nixpkgs with overlays applied
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = overlays;
-      };
-
-    in {
-      homeConfigurations."ezt" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Include your home.nix configuration
-        modules = [ ./home.nix ];
-
-        # Optionally pass additional arguments
-        extraSpecialArgs = {
-          inherit inputs;
-        };
-      };
+    # Import nixpkgs with no Android Studio overlay
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
     };
+  in {
+    # Home Manager configuration
+    homeConfigurations."ezt" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [./home.nix];
+      extraSpecialArgs = {inherit inputs;};
+    };
+  };
 }
-
