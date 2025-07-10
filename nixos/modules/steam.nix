c64ebc01cad423304cda2c9a510024c7205726ac
EzT32 <theodor.berghansen@icloud.com>
2025-07-10 15:03:40 +0200

    steam mangohud

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -1,7 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
+    extest.enable = true;
+
+    package = pkgs.steam.override {
+      extraEnv.MANGOHUD = true;
+      extraPkgs = pkgs: [pkgs.mangohud];
+    };
   };
 }
