cce36ce8ca8e1abc016ab8a1e3b5e47ad6220e79
EzT32 <theodor.berghansen@icloud.com>
2025-07-14 23:11:27 +0200

    Changed kitty font and theme

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -1,10 +1,35 @@
-{...}: {
+{pkgs, ...}:
+let
+  kitty-themes = pkgs.fetchFromGitHub {
+    owner = "dexpota";
+    repo = "kitty-themes";
+    rev = "b1abdd54ba655ef34f75a568d78625981bf1722c";
+    sha256 = "sha256-RcDmZ1fbNX18+X3xCqqdRbD+XYPsgNte1IXUNt6CxIA=";
+  };
+
+  gruvbox-dark = "${kitty-themes}/themes/gruvbox_dark.conf";
+in 
+{
   programs.kitty = {
     enable = true;
+
+    font = {
+      name = "FiraCode Nerd Font";
+      size = 10;
+    };
+
+    extraConfig = builtins.readFile gruvbox-dark;
+
     settings = {
+      window_padding_width = 0;
+      enable_audio_bell = false;
       linux_display_server = "wayland";
       background_opacity = "0.8";
-      background = "#000000";
     };
   };
+
+  home.packages = with pkgs; [
+    nerd-fonts.fira-code
+  ];
 }
+
