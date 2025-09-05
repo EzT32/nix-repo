72a55a285bc716c593a4f2cae678aac4c25a44ae
EzT32 <theodor.berghansen@icloud.com>
2025-09-05 17:45:50 +0200

    fix: change audio profile for BlackShark headset on startup

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -11,4 +11,19 @@
       support32Bit = true;
     };
   };
+  
+  environment.etc."wireplumber/main.lua.d/51-razer.lua".text = ''
+    rule = {
+      matches = {
+        {
+          { "device.name", "matches", "alsa_card.usb-Razer_Razer_BlackShark_V2_Pro*" },
+        },
+      },
+      apply_properties = {
+        ["device.profile"] = "output:iec958-stereo+input:mono-fallback",
+      },
+    }
+
+    table.insert(alsa_monitor.rules, rule)
+  '';
 }
