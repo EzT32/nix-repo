2ddd71a093e5df71b6f802451312ec483c4451a4
EzT32 <theodor.berghansen@icloud.com>
2025-09-08 00:36:39 +0200

    feat: expanded on core cli features

--- modules/programs/core-cli.nix
+++ modules/programs/core-cli.nix
@@ -1,11 +1,18 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    curl
+    evtest
+    htop
+    jq
+    libinput
+    lsof
+    parted
+    pwvucontrol
+    strace
+    unzip
     vim
     wget
     zip
-    unzip
-    libinput
-    evtest
   ];
 }
